d5df32f4fc372a8cf5f1b9580ec18db9efa761a9
Soulivanh Thao <sthao@camelot.ipsl.polytechnique.fr>
2021-02-18 15:53:27 +0100

    start refactoring

--- 1_prep_donnees_downscal/1_prep_donnees_downscal.R
+++ 1_prep_donnees_downscal/1_prep_donnees_downscal.R
@@ -1,7 +1,7 @@
 ## Functions scripts
-source("Comp_func_VM.R")
+source("../Comp_func_VM.R")
 ## PATHS
-source("Setpath.R")
+source("../Setpath.R")
 
 
 
@@ -29,55 +29,65 @@ prep_donnees_downscal<-function(area,year,month.num)
   ##	Set input filename
 
   # si l'aire d'étude est definie
-  if(!is.null(area))
-  {
+  if(!is.null(area)) {
     filename <-  paste0(bdd_data,"/CALIPSO_SAPHIR_CLOUDSAT_",year,"-",month.num,"_",dt,"_",dx,"_",area,"_","speed_RH_SR-OPAQ-PHASE_RF_allvar.dat")
-  }
-  
-  # si l'aire d'étude n'est pas definie
-  if(is.null(area))
-  {
+  } else {
     filename <- paste0(bdd_data,"/CALIPSO_SAPHIR_CLOUDSAT_",year,"-",month.num,"_",dt,"_",dx,"_","speed_RH_SR-OPAQ-PHASE_RF_allvar.dat")
   }
   
   # Ouverture des fichiers
-  if(!file.exists(filename))
-  {
-    next
-  }
-  match.CALIPSO.SAPHIR.CLOUDSAT <- read.table(filename,header=TRUE,sep=";", dec = ".")  # pour toutes les donnees
+  stopifnot(file.exists(filename))
+  match_df <- read.table(filename,header=TRUE,sep=";", dec = ".")  # pour toutes les donnees
   
   # pour connaître le nombre de lignes initiales (donc de données CALIPSO)
-  print(paste("nb initial obs",nrow(match.CALIPSO.SAPHIR.CLOUDSAT)))
+  print(paste("nb initial obs", nrow(match_df)))
   
   
-  #### reorganisation du fichier match.CALIPSO.SAPHIR.CLOUDSAT
-  #pour remettre les colonnes de RR dans le bon ordre : du sol vers l atmosphere
-  match.CALIPSO.SAPHIR.CLOUDSAT<-match.CALIPSO.SAPHIR.CLOUDSAT[,c(1:178,284:179,285:286)]
-  #pour convertir les heures, eviter les doublons et indicer les lignes
-  match.CALIPSO.SAPHIR.CLOUDSAT$mlstdate.SAPHIR <- UTCtolocal(match.CALIPSO.SAPHIR.CLOUDSAT$date.SAPHIR,match.CALIPSO.SAPHIR.CLOUDSAT$time.SAPHIR,match.CALIPSO.SAPHIR.CLOUDSAT$lon.SAPHIR)
-  match.CALIPSO.SAPHIR.CLOUDSAT$mlsttime.frac.SAPHIR <- hourtofrac(match.CALIPSO.SAPHIR.CLOUDSAT$mlstdate.SAPHIR)
-  match.CALIPSO.SAPHIR.CLOUDSAT$mlstdate.CLOUDSAT <- UTCtolocal(match.CALIPSO.SAPHIR.CLOUDSAT$date.CLOUDSAT,match.CALIPSO.SAPHIR.CLOUDSAT$time.CLOUDSAT,match.CALIPSO.SAPHIR.CLOUDSAT$lon.CLOUDSAT)
-  match.CALIPSO.SAPHIR.CLOUDSAT$mlsttime.frac.CLOUDSAT <- hourtofrac(match.CALIPSO.SAPHIR.CLOUDSAT$mlstdate.CLOUDSAT)
-  match.CALIPSO.SAPHIR.CLOUDSAT <- match.CALIPSO.SAPHIR.CLOUDSAT[!duplicated(match.CALIPSO.SAPHIR.CLOUDSAT),]
-  INDICE<-1: nrow(match.CALIPSO.SAPHIR.CLOUDSAT)
-  match.CALIPSO.SAPHIR.CLOUDSAT<-cbind(INDICE,match.CALIPSO.SAPHIR.CLOUDSAT)
+  #### reorganisation du fichier match_df
+
+  # pour remettre les colonnes de RR dans le bon ordre : du sol vers l atmosphere
+  match_df<-match_df[,c(1:178,284:179,285:286)]
   
-  ### Attribution d'un indice a chaque PIXEL SAPHIR!
+  # pour convertir les heures
+  match_df$mlstdate.SAPHIR <- UTCtolocal(
+    match_df$date.SAPHIR, 
+    match_df$time.SAPHIR, 
+    match_df$lon.SAPHIR
+  )
+  match_df$mlsttime.frac.SAPHIR <- hourtofrac(match_df$mlstdate.SAPHIR)
+
+  match_df$mlstdate.CLOUDSAT <- UTCtolocal(
+    match_df$date.CLOUDSAT,
+    match_df$time.CLOUDSAT,
+    match_df$lon.CLOUDSAT
+  )
+  match_df$mlsttime.frac.CLOUDSAT <- hourtofrac(match_df$mlstdate.CLOUDSAT)
+  
+  
+  # Remove duplicatea indicer les lignes
+  match_df <- match_df[!duplicated(match_df),]
+  
+  # Add line index
+  INDICE <- 1:nrow(match_df)
+  match_df <- cbind(INDICE, match_df)
+  
+  ### Attribution d'un indentifiant a chaque PIXEL SAPHIR!
+
   # creation d une colonne avec les variables permettant de differencier chaque pixel SAPHIR
-  match.CALIPSO.SAPHIR.CLOUDSAT$pix_SAPHIR<- paste(match.CALIPSO.SAPHIR.CLOUDSAT$lat.SAPHIR, match.CALIPSO.SAPHIR.CLOUDSAT$lon.SAPHIR, match.CALIPSO.SAPHIR.CLOUDSAT$date.SAPHIR,match.CALIPSO.SAPHIR.CLOUDSAT$time.SAPHIR, sep="_")
+  pix_SAPHIR <- paste(
+    match_df$lat.SAPHIR,
+    match_df$lon.SAPHIR,
+    match_df$date.SAPHIR, 
+    match_df$time.SAPHIR,
+    sep="_"
+  )
+
   # attribution d un indice a chaque pixel SAPHIR 
-  pix_temp<-unique(match.CALIPSO.SAPHIR.CLOUDSAT$pix_SAPHIR)
-  tab_avec_ind_SAPH<-data.frame()
-  for(pix in 1:length(pix_temp))
-  {
-    find_pix<-which(pix_temp[pix]==match.CALIPSO.SAPHIR.CLOUDSAT$pix_SAPHIR)
-    selec_temp<-match.CALIPSO.SAPHIR.CLOUDSAT[find_pix,]
-    selec_temp$IND_pix_SAPHIR<- pix
-    tab_avec_ind_SAPH<-rbind(tab_avec_ind_SAPH, data.frame(selec_temp))
-  }
-  
-  match.CALIPSO.SAPHIR.CLOUDSAT<-tab_avec_ind_SAPH
+  match_df <- cbind(
+    IND_pix_SAPHIR = factor(pix_SAPHIR, levels = pix_SAPHIR),
+    match_df
+  )
+  rm(pix_SAPHIR)
   
   
   
@@ -86,40 +96,19 @@ prep_donnees_downscal<-function(area,year,month.num)
   #### ne garder que les valeurs de jour pour les 3 SATELLITES (car pas de donnees CloudSat (RR) la nuit)
   
   # selection des donnees de jour
-  index_NA<-which(rowSums(is.na(match.CALIPSO.SAPHIR.CLOUDSAT[,180:285]))!=106)   #### pour avoir les index des lignes CloudSat qui ne sont pas entierement en NA (lignes CloudSat entierement en NA= donnees de nuit) 
-  data_day<-match.CALIPSO.SAPHIR.CLOUDSAT[index_NA,]                              #### pour garder juste les donnees de jours
-  data_day<-data_day[order(data_day$INDICE,decreasing=F),]   #sort sur les indices au cas ou la selection aiet melangé les donnees
+  i_RR <- grep("RR", colnames(match_df))
+  stopifnot(sum(iRR) == 106)
+  i_NA <- (rowMeans(is.na(match_df[, i_RR])) == 1)   #### pour avoir les index des lignes CloudSat qui ne sont pas entierement en NA (lignes CloudSat entierement en NA= donnees de nuit) 
+  match_df <- match_df[!i_NA,]                              #### pour garder juste les donnees de jours
   
   # selection et controle des donnees de SAPHIR
   print("set SAPHIR variables")
-  RH_L1 <- data_day[,c(grep("rh1",colnames(data_day)))]
-  RH_L2 <- data_day[,c(grep("rh2",colnames(data_day)))]
-  RH_L3 <- data_day[,c(grep("rh3",colnames(data_day)))]
-  RH_L4 <- data_day[,c(grep("rh4",colnames(data_day)))]
-  RH_L5 <- data_day[,c(grep("rh5",colnames(data_day)))]
-  RH_L6 <- data_day[,c(grep("rh6",colnames(data_day)))]
+  i_RH <- grep("^rh[1-6]$", colnames(match_df))
+  i_URH <- grep("^rh\.unc[1-6]$",colnames(match_df))
   
-  RH <- cbind(RH_L1,RH_L2,RH_L3,RH_L4,RH_L5,RH_L6)
-  
-  URH_L1 <- data_day[,c(grep("rh.unc1",colnames(data_day)))]
-  URH_L2 <- data_day[,c(grep("rh.unc2",colnames(data_day)))]
-  URH_L3 <- data_day[,c(grep("rh.unc3",colnames(data_day)))]
-  URH_L4 <- data_day[,c(grep("rh.unc4",colnames(data_day)))]
-  URH_L5 <- data_day[,c(grep("rh.unc5",colnames(data_day)))]
-  URH_L6 <- data_day[,c(grep("rh.unc6",colnames(data_day)))]
-  
-  URH <- cbind(URH_L1,URH_L2,URH_L3,URH_L4,URH_L5,URH_L6)
-  
-  #SAPHIR<- match.CALIPSO.SAPHIR.CLOUDSAT[,132:137]
-  complete_row_RH <- which(complete.cases(RH))  # trouver les lignes de RH completes 
-  data_day<-data_day[complete_row_RH,]  # correspondance avec les données jours disponibles : selectionner les lignes de data_day correspondantes aux lignes de RH completes
-  RH<-as.matrix(data_day[,132:137])
-  sum(is.na(RH))
-  range(RH)
-  URH<-as.matrix(data_day[, 144:149])
-  range(URH)
-  sum(is.na(URH))
-  
+  #SAPHIR<- match_df[,132:137]
+  complete_row_RH <- complete.cases(match_df[, i_RH])  # trouver les lignes de RH completes 
+  match_df <- match_df[complete_row_RH,]  # correspondance avec les données jours disponibles : selectionner les lignes de data_day correspondantes aux lignes de RH completes
   
   ##	Set Extra variables
   
@@ -133,33 +122,31 @@ prep_donnees_downscal<-function(area,year,month.num)
   
   # selection et controle des donnees jour de CLOUDSAT
   print("set CLOUDSAT variables")
-  RR2<-data_day[,180:284]
   # remplacement des NA de RR par -30
-  RR2[is.na(RR2)] <- (-30)
   # remplacement des valeurs  inférieures à -30 par -30
-  RR2[RR2<=(-30)] <- (-30)
-  
-  RR2_indices<-data_day[,1]
-  RR2<-cbind(RR2_indices,RR2, data_day[,176:177])
+  i_minus30 <- is.na(match_df[, i_RH]) | (match_df[, i_RH] <= -30)
+  match_df[, i_RH][i_minus30] <- -30
   
   #moyenne des RR avec au moins 2 obs
-  layer_RR<-c(1:25)
+  layer_RR <- 1:25
   
   RR<-RR2[,c(-1,-107:-109)]  ## agregation jusqu a 24 km (inclus), le km 25 ne comporte qu une couche de 240m, il est donc exclu du calcul ci apres
   
-  RR_DF_moylayer<-data.frame()
-  for(n in 1:(length(layer_RR)))
+  RR_DF_moylayer <- matrix(NA, nrow = nrow(match_df), ncol = length(layer_RR))
+  valt <- as.numeric(str_sub(colnames(match_df[, i_RR]),3))
+  for(l in layer_RR)
   {
-    print(n)
-    valt <- as.numeric(str_sub(colnames(RR),3))
-    if(layer_RR[n]==1){ RR_mean<-RR[,(valt >= 1.2) & (valt <= 2)] }
-    else
-    {
-      RR_mean<-RR[,(valt >= layer_RR[n]) & (valt <= layer_RR[n+1])] 
+    print(l)
+    if(l == 1){
+      RR_mean <- RR[, (valt >= 1.2) & (valt <= 2)] 
+    } else {
+      RR_mean < -RR[, (valt >= layer_RR[n]) & (valt <= layer_RR[n+1])] 
     }
-    for(lgn in 1 :length(RR_mean))
-    {
-      if(sum(is.na(RR_mean[lgn,]))<=2){moy<-rowMeans(RR_mean, na.rm = TRUE)}
+
+    for(lgn in 1: nrow(RR_mean)) {
+      if(sum(is.na(RR_mean[lgn,])) <= 2){
+        moy <- rowMeans(RR_mean, na.rm = TRUE)
+      }
     }
     if(n==1){RR_DF_moylayer<-moy}    
     else
@@ -256,7 +243,7 @@ prep_donnees_downscal<-function(area,year,month.num)
   sum(is.na(SR))
   
   
-  ######### creation de la nouvelle table match.CALIPSO.SAPHIR.CLOUDSAT nettoyee a utiliser pour le script de downscalling
+  ######### creation de la nouvelle table match_df nettoyee a utiliser pour le script de downscalling
   coords.sp.CALIPSO <- data_day[ , c("lon.CALIPSO","lat.CALIPSO")]
   coords.time.CALIPSO <- as.POSIXct(paste(data_day[ , "date.CALIPSO"], data_day[ , "time.CALIPSO"], sep =" "),tz="UTC",format="%Y-%m-%d %H:%M:%S")
   coords.sp.SAPHIR <- data_day[ , c("lon.SAPHIR","lat.SAPHIR")]
@@ -266,8 +253,8 @@ prep_donnees_downscal<-function(area,year,month.num)
   matrix_coord<-data.frame(Indice=data_day[ , "INDICE"] ,time.CALIPSO=as.POSIXct(coords.time.CALIPSO),coords.sp.CALIPSO,time.SAPHIR=as.POSIXct(coords.time.SAPHIR),coords.sp.SAPHIR,time.CLOUDSAT=as.POSIXct(coords.time.CLOUDSAT),coords.sp.CLOUDSAT,IND.SAPHIR=IND.SAPHIR,IND_pix_SAPHIR=IND_pix_SAPHIR)
   matrix_coord<-matrix_coord[complete_row_SR,]
   
-  match.CALIPSO.SAPHIR.CLOUDSAT2<-cbind(SR,RR_DF_moylayer2,RH,URH,matrix_coord)
-  write.table(match.CALIPSO.SAPHIR.CLOUDSAT2,paste0(bdd_data,"/CALIPSO_SAPHIR_CLOUDSAT_",year,"-",month.num,"_",dt,"_",dx,"_",area,"_",
+  match_df2<-cbind(SR,RR_DF_moylayer2,RH,URH,matrix_coord)
+  write.table(match_df2,paste0(bdd_data,"/CALIPSO_SAPHIR_CLOUDSAT_",year,"-",month.num,"_",dt,"_",dx,"_",area,"_",
                                                     "for_downscal.dat"),col.names=TRUE,row.names=FALSE,append=TRUE,sep=";",quote=FALSE)
   
 }#fin de la fonction
